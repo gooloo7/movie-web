ebe26577f70a19747c88ae8d0b857fe77910363b
BuildTools <unconfigured@null.spigotmc.org>
2017-06-22 23:55:11 +0200

    added more animations to every site

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("trailer-button");
-var ytvideo = document.getElementById("youtube-video");
-
-button.onclick = function () {
-    button.classList.add("trailer-opacity");
-    ytvideo.classList.add('video-opacity');
-}
